83a996dc9e312e21bdaaeb5da4a016dda0b26b29
dietrich <dietrich@newskylabs.net>
2019-10-22 18:44:14 +0200

    Added file tree sceleton for datasets

---
+++ newskylabs/datasets/kaggle_kuzushiji_recognition/dataset.py
@@ -0,0 +1,27 @@
+"""newskylabs/datasets/kaggle_kuzushiji_recognition/dataset.py:
+
+See:
+
+  - Kuzushiji Recognition
+    Opening the door to a thousand years of Japanese culture
+    https://www.kaggle.com/c/kuzushiji-recognition/
+
+"""
+
+__author__      = "Dietrich Bollmann"
+__email__       = "dietrich@formgames.org"
+__copyright__   = "Copyright 2018 Dietrich Bollmann"
+__license__     = "Apache License 2.0, http://www.apache.org/licenses/LICENSE-2.0"
+__date__        = "2019/10/22"
+
+import numpy as np
+
+## =========================================================
+## Dataset
+## ---------------------------------------------------------
+
+## =========================================================
+## =========================================================
+
+## fin.
+
---
+++ newskylabs/datasets/kaggle_kuzushiji_recognition/download.py
@@ -0,0 +1,26 @@
+"""newskylabs/datasets/kaggle_kuzushiji_recognition/download.py:
+
+See:
+
+  - Kuzushiji Recognition
+    Opening the door to a thousand years of Japanese culture
+    https://www.kaggle.com/c/kuzushiji-recognition/
+
+"""
+
+__author__      = "Dietrich Bollmann"
+__email__       = "dietrich@formgames.org"
+__copyright__   = "Copyright 2018 Dietrich Bollmann"
+__license__     = "Apache License 2.0, http://www.apache.org/licenses/LICENSE-2.0"
+__date__        = "2018/02/22"
+
+import numpy as np
+
+## =========================================================
+## Dataset
+## ---------------------------------------------------------
+
+## =========================================================
+## =========================================================
+
+## fin.
